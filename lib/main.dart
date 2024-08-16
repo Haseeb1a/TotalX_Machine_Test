337838bef6a2acbccce5f9229591b6d7067f3030
Haseeb1a <mh3824555@gmail.com>
2024-08-17 01:00:59 +0530

    Firebase_Confilgure_Setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:totalxtest/controller/home_user_controller.dart';
+import 'package:totalxtest/firebase_options.dart';
+import 'package:totalxtest/view/auth/login_page.dart';
 // import 'package:totalxtest/firebase_options.dart';
 // import 'package:totalxtest/firebase_options.dart';
 import 'package:totalxtest/view/home/home_page.dart';
@@ -9,7 +11,7 @@ import 'package:totalxtest/view/home/home_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
@@ -30,7 +32,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
         ),
-        home: const Homepage(),
+        home: const Loginpage(),
       ),
     );
   }
